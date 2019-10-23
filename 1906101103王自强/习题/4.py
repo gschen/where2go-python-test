337930f8d9332d9eabe813d69da51552a4fdfea4
wangziqiang2 <1840957110@qq.com>
2019-10-23 20:19:32 +0800

    习题5,6,7

--- 1906101103王自强/习题/4.py
+++ 1906101103王自强/习题/4.py
@@ -1,9 +1,10 @@
-n=int(input('请输入你要求的项数:'))
-n_2=0
-n_1=1
-x=1
-for i in range(2,n+1):
-    x=n_2+n_1
-    n_2=n_1
-    n_1=x
-print('第{}项数是{}'.format(n,x))
+while True:
+    n=int(input('请输入你要求的项数:'))
+    n2=0
+    n1=1
+    x=0
+    for i in range(2,n+1):
+        x=n2+n1
+        n2=n1
+        n1=x
+    print('第{}项数是{}'.format(n,x))
