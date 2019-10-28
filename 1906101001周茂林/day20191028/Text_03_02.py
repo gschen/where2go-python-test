e218a9a896cb7f1e7c2d280dee202fcd4f2d180a
zhoumaolin1 <2915133868@qq.com>
2019-10-28 20:18:52 +0800

    0

--- 1906101001周茂林/day20191028/Text_03_02.py
+++ 1906101001周茂林/day20191028/Text_03_02.py
@@ -13,17 +13,15 @@ for i in range(1,n+1):
         list4.append(a)
         continue
     list1 = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
-    b = c = 0
-    for i in list1:
-        b += i
-    for m in a2:
-        c += int(m)
-    z = (b + c) % 11
+    b = 0
+    for i in range(17):
+        b += int(a[i]) * list1[i]
+    z = b % 11
     list2 = [0,1,2,3,4,5,6,7,8,9,10]
     list3 = ['1','0','x','9','8','7','6','5','4','3','2']
-    d = list2.index(z)
-    e = list3[d]
-    if e != a[17]:
+    c = list2.index(z)
+    d = list3[c]
+    if d != a[17]:
         list4.append(a)
 if n2 ==n:
     if len(list4) == 0:
