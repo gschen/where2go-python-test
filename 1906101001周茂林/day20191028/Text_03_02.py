bda155f44e05f5b0ac42088fc2c3eb29403794fa
zhoumaolin1 <2915133868@qq.com>
2019-10-28 20:03:03 +0800

    0

---
+++ 1906101001周茂林/day20191028/Text_03_02.py
@@ -0,0 +1,42 @@
+n = int(input())
+list4 = []
+n2 = 0
+for i in range(1,n+1):
+    n2 += 1
+    a = input()
+    a2 = a[:16]
+    a3 = 0
+    for i in a2:
+        if i.isdigit():
+            a3 += 1
+    if a3 < 16:
+        list4.append(a)
+        continue
+    list1 = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+    b = c = 0
+    for i in list1:
+        b += i
+    for m in a2:
+        c += int(m)
+    z = (b + c) % 11
+    list2 = [0,1,2,3,4,5,6,7,8,9,10]
+    list3 = ['1','0','x','9','8','7','6','5','4','3','2']
+    d = list2.index(z)
+    e = list3[d]
+    if e != a[17]:
+        list4.append(a)
+if n2 ==n:
+    if len(list4) == 0:
+        print('All passed')
+    else:
+        for i in list4:
+            print(i)
+
+
+
+'''
+        320124198808240056
+        12010X198901011234
+        110108196711301866
+        37070419881216001X
+'''
