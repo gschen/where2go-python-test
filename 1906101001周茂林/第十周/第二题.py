b59ee0e2fa1b95a6f5657eb4c57dcbb625455ac7
zhoumaolin1 <2915133868@qq.com>
2019-10-29 21:51:40 +0800

    0

--- 1906101001周茂林/第十周/第二题.py
+++ 1906101001周茂林/第十周/第二题.py
@@ -2,9 +2,9 @@
 有n个人围成一圈，顺序排号。
 从第一个人开始报数（从1到3报数），凡报到3的人退出圈子，问最后留下的是原来第几号的那位。
 '''
-import numpy as np:
+import numpy as np
 def qq(number):
-    number = list(range(1,n+1))
+    number = list(range(1,number+1))
     aa = len(number)
     bb = []
     while aa == 2:
@@ -14,3 +14,4 @@ def qq(number):
         npaa = np.array(aa)
         aa = npaa - npbb
     print(aa[2])
+
