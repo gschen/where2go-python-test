5d8adb00289d1fe4f4b2e9e2913ac87c646d3668
wangziqiang2 <1840957110@qq.com>
2019-10-31 22:33:41 +0800

    习题2

--- 1906101103王自强/9周习题/10.py
+++ 1906101103王自强/9周习题/10.py
@@ -1,18 +1,8 @@
 n = int(input('请输入你要求第几项和:'))
-n2=1
-m2=0
-n1=1
-m1=1
-z2=0
-for i in range(1,n+1):
-    y=m2+m1
-    x=n2+n1
-    m2=m1
-    n2=n1
-    m1=y
-    n1=x
-    z1=x/y
-    z3=z2+z1
-    z2=z1
-print('前{}项和为{}'.format(n,z3))
-#10
+a=2
+b=1
+c=a/b
+for i in range(n-1):
+    a,b=a+b,a
+    c+=a/b
+print(c)
--- 1906101103王自强/9周习题/9.py
+++ 1906101103王自强/9周习题/9.py
@@ -1,7 +1,10 @@
-m=['a','b','c']
-for i in range(0,3):
-    for o in range(0,3):
-        for p in range(0,3):
-            if i!=0 and o!=0 and p!=2 and i!=o!=p:
-                print('{}对x,{}对y，{}对z'.format(m[i],m[o],m[p]))
-#9
+'''9、	两个乒乓球队进行比赛，各出三人。甲队为a,b,c三人，乙队为x,y,z三人
+。已抽签决定比赛名单。有人向队员打听比赛的名单。a说他不和x比，c说他不和x
+,z比，请编程序找出三队赛手的名单'''
+
+l=['x','y','z']
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            if i!=0 and k!=0 and k!=2and i!=j and j!=k and i!=k:
+                print('a对{}，b对{}，c对{}'.format(l[i],l[j],l[k]))
