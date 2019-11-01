16b07efd6b10ea0b3c5067c497bfe6a5e4f36e90
zhoumaolin1 <2915133868@qq.com>
2019-11-01 13:46:04 +0800

    0

--- 1906101001周茂林/第十周/第七题.py
+++ 1906101001周茂林/第十周/第七题.py
@@ -1,10 +1,12 @@
 '''
 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
 '''
-l = 'abcabcbb'
-l2 = []
-for m in range(len(l)+1):
-    for n in range(len(l)+1):
-        if m < n and len(l[m:n]) == len(set(l[m:n])):
-            l2.append(len(l[m:n]))
-print(max(l2))
+def ababab(l):
+    l2 = []
+    for m in range(len(l)+1):
+        for n in range(len(l)+1):
+            if m < n and len(l[m:n]) == len(set(l[m:n])):
+                l2.append(len(l[m:n]))
+    print(max(l2))
+
+ababab('abcabcbb')
