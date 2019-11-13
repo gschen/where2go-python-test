d1ab7871c04e4d5776eae9f4be853f10f5cc8d96
wangziqiang2 <1840957110@qq.com>
2019-11-13 21:53:19 +0800

    练习1

--- 1906101103王自强/平时作业/练习.py
+++ 1906101103王自强/平时作业/练习.py
@@ -7,3 +7,4 @@ for a in range(1,10):
                     if (a*10+b)*(c*100+d*10+e)==(a*100+d*10+b)*(c*10+e) and a!=b and a!=c and a!=d and a!=e and b!=c and b!=e and b!=d and b!=e and c!=d and c!=e and d!=e:
                         i+=1
 print(i)
+#。
