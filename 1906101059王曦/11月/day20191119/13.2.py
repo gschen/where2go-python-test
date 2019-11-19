e6d44740372ba5b14c376bafbd989333d0f72817
wangxi2 <2359362331@qq.com>
2019-11-19 22:58:00 +0800

    计算1到M (含M)之间的合数数量,输出其值。

---
+++ 1906101059王曦/11月/day20191119/13.2.py
@@ -0,0 +1,11 @@
+#计算1到M (含M)之间的合数数量,输出其值
+l = []
+m = int(input())
+for i in range(1,m+1):
+    for n in range(2,m):
+        if i%n!=0:
+            break
+        else:
+            l.append(i)
+a =len(set(l))
+print(a)
