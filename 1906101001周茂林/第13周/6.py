5307bc9eed1507f6b91cb5df90e5fa30f8c84552
zhoumaolin1 <2915133868@qq.com>
2019-11-20 21:35:01 +0800

    0

---
+++ 1906101001周茂林/第13周/6.py
@@ -0,0 +1,15 @@
+'''
+对于给出的长度为N的整数数组，对连续3个元素求平方和,输出平方和的最大值。
+输入说明:第-行,数组中元素个数N(N< 1000),第二行是这个数组中的N个元素，间用空格隔开,每个元素小于100000.
+输出说明:输出连续3个元素平方和的最大值。
+输入样例: 8
+1 1 -1 2 -3 3 -4 2
+输出样例: 34
+'''
+N = int(input())
+lis = list(map(int,input().split()))
+lis2 = [x*x for x in lis]
+lis3 = []
+for i in range(len(lis2)-3):
+    lis3.append(lis2[i] + lis2[i+1] + lis2[i+2])
+print(max(lis3))
