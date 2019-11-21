a3e5ab07a64cef4ea08017997907c189b3fd817a
TJH <1476554464@qq.com>
2019-11-21 22:31:29 +0800

    test

--- 1906101056李靖/第9周练习题/5.py
+++ 1906101056李靖/第9周练习题/5.py
@@ -1,7 +1,7 @@
-for x in range(1,10):
-	for y in range(0,10):
-		for z in range(0,10):
-			m=x**3+y**3+z**3
-			n=x*100+y*10+z
+for a in range(1,10):
+	for b in range(0,10):
+		for c in range(0,10):
+			m=a**3+b**3+c**3
+			n=a*100+b*10+c
 			if m==n:
 				print(m)
