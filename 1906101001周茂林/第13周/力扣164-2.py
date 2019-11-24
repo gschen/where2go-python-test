b3adb956911bd00281e0c4925d8e24a8498dea5d
zhoumaolin1 <2915133868@qq.com>
2019-11-24 15:16:04 +0800

    0

--- 1906101001周茂林/第13周/力扣164-2.py
+++ 1906101001周茂林/第13周/力扣164-2.py
@@ -1,19 +1,13 @@
 def countServers(grid):
     n = 0
-    le = len(grid[0])
     for i in grid:
-        m = 0
-        for j in i:
-            if j == 1:
-                m += 1
-        if m > 1:
-            n += m
-    for p in range(len(grid)):
+        if i.count(1) > 1:
+            n += i.count(1)
+    for j in range(len(grid)):
         for g in range(len(grid)):
-            for f in range(le):
-                if p != g and grid[p][f] == grid[g][f] == 1 and 1 not in grid[g][:f] and 1 not in grid[g][f+1:]:
+            for f in range(len(grid[0])):
+                if j != g and grid[j][f] == grid[g][f] == 1 and grid[g].count(1) == 1:
                     n += 1
     print(n)
 
 countServers([[1,1,0,1,0],[0,0,0,0,0],[0,0,0,1,0]])
-
