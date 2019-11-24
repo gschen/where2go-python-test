2878e5d25838f377324078fcb46f481fb7184667
康健铭 <1257035826@qq.com>
2019-11-24 22:23:56 +0800

    0

---
+++ 1906101025康健铭/ago/2.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+kjm=datetime(2000,4,13)
+print(kjm.isoformat())
+print(kjm.year,"年",kjm.month,"月",kjm.day,"日")
+print(kjm.day,"日",kjm.month,"月",kjm.year,"年")
+print(kjm.strftime("%Y-%m-%d"))
+print(kjm.strftime("%d-%m-%Y"))
+print(kjm.strftime("%Y:%m:%d"))
+print(kjm.strftime("%Y=%m=%d"))
+print(kjm.strftime("%Y/%m?%d"))
+print("{0:%Y}年{0:%m}月{0:%d}日".format(kjm))
+print("{0:%d}日{0:%m}月{0:%Y}年".format(kjm))
