94dde1cf45a83a260a01b90d2679adb8e1b3fc4f
LEGION <969970279@qq.com>
2019-11-28 00:59:37 +0800

    测试

--- 1906101045蓝昊杰/培训任务/培训第四题.py
+++ 1906101045蓝昊杰/培训任务/培训第四题.py
@@ -1,17 +1,12 @@
 n=int(input())
-a=b=c=d=0
 while n>=11:
     n-=11
-    a+=1
     print(11)
 while n<11 and n>=5:
     n-=5
-    b+=1
     print(5)
 while n<5 and n>=2:
     n-=2
-    c+=1
     print(2)
 if  n==1:
-    d+=1
     print(1)
