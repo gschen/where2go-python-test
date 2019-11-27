7ed03aa75e002e14067c72e0c3bf34709496673a
lk15244816107 <963385794@qq.com>
2019-11-28 00:20:55 +0800

    第一次作业

--- 1906101049李凯/第一次练习题.py
+++ 1906101049李凯/第一次练习题.py
@@ -1,6 +1,6 @@
 def fact(n):
     if n==1:
-        return 1
+        return n
     else:
-        return n*fact(n-1)
-print（fact(5)）
+        return fact(n-1)*n
+print(fact(5))
--- 1906101049李凯/第一次练习题2.py
+++ 1906101049李凯/第一次练习题2.py
@@ -1,6 +1,6 @@
 def addpower():
-    L=[]
     while n>1:
+        L=[]
         for i in range(2,n+1):
             if n%i==0:
                 n=int(n/i)
