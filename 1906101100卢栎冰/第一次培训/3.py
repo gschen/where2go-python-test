628da9338e384c3cebd709fb5823d8785cd5384b
LIBICE0106 <815442957@qq.com>
2019-12-02 22:43:40 +0800

    ，kjtf

--- 1906101100卢栎冰/第一次培训/3.py
+++ 1906101100卢栎冰/第一次培训/3.py
@@ -1,10 +1,12 @@
-def m(n):
+#分子
+# def m(n):
     a,b=1,2
     while n>0
         a,b=b,a+b
         n=n-1
     return a
 print('第20个分子是：'，m(20))
+#分母
 def k(n):
     a,b=1,1
     while n>0:
@@ -12,5 +14,12 @@ def k(n):
         n=n-1
      return a
 print('第20个分母是：'，k(20))
+#各项和
+def f(x):
+    if x==1:
+        return 2/1
+    else:
+        return(m(x)/k(x)+f(x-1))
+    print(f(20))
 
 
