c6fa5a73a29e8a9059b0261e859b0b88399135f4
wangyan <172328386@qq.com>
2019-12-02 20:13:31 +0800

    关键在于for循环的运用，将题中的a循环到列表中，依次循环。 给定一个数组，你需要删除反复出现的元素，使得每个函数只出现一次，返回移除后数组的新长度和新的数组。输入样例：[1,1,1,2,3,4,4,4]   输入样式：4   [1,2,3,4]

--- 1906101058王妍/test.2.py
+++ 1906101058王妍/test.2.py
@@ -1,1 +1,8 @@
-print([x for x in range(1,101)])
+ls=[1,1,1,2,3,4,4,4]
+for a in range(len(ls)-1):
+    for b in ls[a+1:]:
+        if ls[a]==b:
+            ls.remove(ls[a])
+        else:
+            break
+print(len(ls))
