7028221904213098f6de3e254969ac408fe453be
LIBICE0106 <815442957@qq.com>
2019-12-02 22:26:10 +0800

    ，kjtf

---
+++ 1906101100卢栎冰/第一次培训/3.py
@@ -0,0 +1,16 @@
+def m(n):
+    a,b=1,2
+    while n>0
+        a,b=b,a+b
+        n=n-1
+    return a
+print('第20个分子是：'，m(20))
+def k(n):
+    a,b=1,1
+    while n>0:
+        a,b=b,a+b
+        n=n-1
+     return a
+print('第20个分母是：'，k(20))
+
+
