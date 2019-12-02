21d17e5ab903cf4b85f08a0cc10ff69071a177ce
duyunjie <2715657901@qq,com>
2019-12-02 19:18:47 +0800

    测试

---
+++ 1906101070杜云洁/第十四周作业/1.py
@@ -0,0 +1,9 @@
+nums=[1,1,1,2,3,4,4,4]
+for i in range(len(nums)-1):
+    for a in nums[i+1: ]:
+        if nums[i]==a:
+            nums.remove(nums[i])
+        else:
+            break
+print(len(nums))
+print(nums)
