f2da77c45d4e23f87352f7269f385f5cce4671d1
wuyichen1 <1532409005@qq.com>
2019-12-02 23:37:30 +0800

    test

--- 1906101084吴怡辰/第一次培训/课后test/5.py
+++ 1906101084吴怡辰/第一次培训/课后test/5.py
@@ -1,8 +1,7 @@
 #5. 现知道有些数为回文数，例如121，242，12321。我们就称这种数字为回文数，像10，14，467就不为回文数。先要求你用程序将区间[1，100000]内所有的回文数输出。
-count = 0
-for i in range(1,100001):
-    a = str(i)
-    if str(i) == str(i)[::-1]:
-        count += 1
+def hws(s):
+    return s[::-1] == s
+
+for i in range(10000):
+    if (hws(str(i))):
         print(i)
-print(count)
