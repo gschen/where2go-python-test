90550106da6e64f355f9ee9ad17ffb43b4042287
wangyan <172328386@qq.com>
2019-12-02 21:58:35 +0800

    test

--- 1906101058王妍/第十四周训练题/test1.py
+++ 1906101058王妍/第十四周训练题/test1.py
@@ -1,9 +1,9 @@
-ls=[1,1,1,2,3,4,4,4]
-for x in range(len(ls)-1):
-    for y in Ls[x+1: ]:
-        if ls[x] == y:
-            ls.remove(ls[x])
+l=[1,1,1,2,3,4,4,4]
+for x in range(len(l)-1):
+    for y in l[x+1: ]:
+        if l[x] == y:
+            l.remove(l[x])
         else:
             break
-print(len(ls))
-print(ls)
+print(len(l))
+print(l)
