1fe9962542f25c7c93a2dccd6a6ccaa43dffe3a7
LIBICE0106 <815442957@qq.com>
2019-12-02 22:47:07 +0800

    ，kjtf

--- 1906101100卢栎冰/第一次培训/2.py
+++ 1906101100卢栎冰/第一次培训/2.py
@@ -1,14 +1,12 @@
-L=[]
-for a in range(1,10):
-    for b in range(1,10):
-        for c in range(1,10):
-            for d in range(1,10):
-                for e in range(1,10):
-                    if a!=b and a!=c and a!=d and a!=e and b!=c and b!=d and b!=e and c!=d and c!=e and d!=e:
-                        if(a*10+b)*(c*100+d*10+e)==(a*100+d*10+b)*(c*10+e):
-                            l=[a*10+b,c*100+d*10+e,a*100+d*10+b,c*10+e]
-                            for i in l:
-                                l.append(i)
+l = []
+for a in range(1, 10):
+    for b in range(1, 10):
+        for c in range(1, 10):
+            for d in range(1, 10):
+                for e in range(1, 10):
+                    if a != b and a != c and a != d and a != e and b != c and b != d and b != e and c != d and c != e and d != e:
+                        if ((a * 10 + b) * (c * 100 + d * 10 + e)) % ((a * 100 + d * 10 + b) * (c * 10 + e)) == 0:
+                            L = str(a * 10 + b)
+                            l.append(L)
 
-m=len(l)/4
-print('如此计算能有的种类数：%0.f' %m)
+print(len(l))
