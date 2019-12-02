f35f6ad2d931f5951a6907d8883d8ca04da3034c
jiangwanglin <917563885@qq.com>
2019-12-02 22:10:01 +0800

    这里是提交代码，，，这里面写代码的介绍

--- 1906101115江汪霖/第十四周培训练习题.py
+++ 1906101115江汪霖/第十四周培训练习题.py
@@ -1,55 +1,44 @@
-# L=[1,1,1,2,3,4,4,4]
-# L.pop(0)
-# L.pop(1)
-# L.pop(3)
-# L.pop(4)
-# a=len(L)
-# print(a,L)
-# a = 2
-# b = 1
-# sum = 0
-# for i in range(20):
-#     sum = a / b + sum
-#     a, b = (a + b), a
-# print(sum)
-# x=0
-# for a in range(1,10):
-#     for b in range(1,10):
-#         for c in range(1,10):
-#             for d in range(1,10):
-#                 for e in range(1,10):
-#                     if a!=b and a!=c and a!=d and a!=e and b!=c and b!=d and b!=e and c!=d and c!=e and d!=e and(a*10+b)*(c*100+d*10+e)==(a*100+d*10+b)*(c*10+e):
-#                         x+=1
-# print(x)
-# x=int(input(""))
-# a=x//11
-# b=(x-a*11)//5
-# c=(x-(a*11+b*5))//2
-# e=(x-(a*11+b*5+c*2))
-# print("11\n"*a)
-# print("5\n"*b)
-# print("2\n"*c)
-# print("1\n"*e)
-# while a>0:
-#     print('11')
-#     a-=1
-# while b > 0:
-#     print('5')
-#     b -= 1
-# while c>0:
-#     print('2')
-#     c-=1#c=c-1
-# while e>0:
-#     print('1')
-#     e-=1
-for i in [11,10000]:
-    if i<100 and i%11==0 :
-        print(i)
-    if i>100 and i<1000:
-        if (i//100)==(i%10):
-            print(i)
-    if i >1000 and i<10000:
-        if (i//1000)==(i%100) and (i-((i//1000)*1000)//100)==((i-(i//1000)*1000+(i//100)*100)//10):
-            print(i)
-    if i>10000 and i<100000:
-        if (i//10000)==(i%1000) and ()
+L=[1,1,1,2,3,4,4,4]
+L.pop(0)
+L.pop(1)
+L.pop(3)
+L.pop(4)
+a=len(L)
+print(a,L)
+a = 2
+b = 1
+sum = 0
+for i in range(20):
+    sum = a / b + sum
+    a, b = (a + b), a
+print(sum)
+x=0
+for a in range(1,10):
+    for b in range(1,10):
+        for c in range(1,10):
+            for d in range(1,10):
+                for e in range(1,10):
+                    if a!=b and a!=c and a!=d and a!=e and b!=c and b!=d and b!=e and c!=d and c!=e and d!=e and(a*10+b)*(c*100+d*10+e)==(a*100+d*10+b)*(c*10+e):
+                        x+=1
+print(x)
+x=int(input(""))
+a=x//11
+b=(x-a*11)//5
+c=(x-(a*11+b*5))//2
+e=(x-(a*11+b*5+c*2))
+print("11\n"*a)
+print("5\n"*b)
+print("2\n"*c)
+print("1\n"*e)
+while a>0:
+    print('11')
+    a-=1
+while b > 0:
+    print('5')
+    b -= 1
+while c>0:
+    print('2')
+    c-=1#c=c-1
+while e>0:
+    print('1')
+    e-=1
