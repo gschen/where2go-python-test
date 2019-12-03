301b987caa474ff85bfad49a24b1e47bd5a36519
wuyichen1 <1532409005@qq.com>
2019-12-03 14:56:07 +0800

    test

--- 1906101084吴怡辰/第一次培训/课后test/3递归求和.py
+++ 1906101084吴怡辰/第一次培训/课后test/3递归求和.py
@@ -1,9 +1,10 @@
 #3. 有一分数序列:2/1,3/2,5/3,8/5,13/8,21/13 求出这个数列的前20项之和
 #提示：用递归
-
-def f(n):
-    if n==1:
-        return 2/1
-    if n>1:
-        return(1/f(n-1)+1)+f(n-1)
-print(f(20))
+n=int(input("输入项数："))
+a=2
+b=1
+sum=0
+for i in range(n):
+    sum = sum + (a / b)
+    a,b = a+b,a
+print(sum)
