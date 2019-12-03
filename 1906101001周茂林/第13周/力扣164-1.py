78da2e2cb0c48b50940d4c4fb553c59a8a1b85f9
zhoumaolin1 <2915133868@qq.com>
2019-12-03 22:37:10 +0800

    0

--- 1906101001周茂林/第13周/力扣164-1.py
+++ 1906101001周茂林/第13周/力扣164-1.py
@@ -1,17 +1,13 @@
-class Solution:
-    def minTimeToVisitAllPoints(points):
-        lis1 = []
-        lis2 = []
-        for i in points:
-            lis1.append(i[0])
-            lis2.append(i[1])
-        m,n = 0,0
-        for j in range(len(lis1)-1):
-            m += abs(lis1[j]-lis1[j+1])
-        for k in range(len(lis2)-1):
-            n += abs(lis2[k]-lis2[k+1])
-        if m >= n:
-            print(m)
-        else:
-            print(n)
-    minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]])
+def baa(points):
+    lis1 = []
+    lis2 = []
+    for i in points:
+        lis1.append(i[0])
+        lis2.append(i[1])
+    n = 0
+    for j in range(len(lis1)-1):
+        n += max(abs(lis1[j]-lis1[j+1]), abs(lis2[j]-lis2[j+1]))
+    print(n)
+
+
+baa([[1, 1], [3, 4], [-1, 0]])
