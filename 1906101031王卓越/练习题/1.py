e697c65c1bb3dd0bae78b01615e998efcf8e081d
wangzhuoyue <2315753700@qq.com>
2019-12-03 19:11:24 +0800

    15周

---
+++ 1906101031王卓越/练习题/1.py
@@ -0,0 +1,22 @@
+#15周课堂作业
+def raw(l,n,q,k):
+    if q<len(l):
+        a=l[q][0]-l[n][0]
+        b=l[q][1]-l[n][1]
+        if abs(a)>abs(b) and q<len(l):
+            k=k+abs(a)
+            return raw(l,n+1,q+1,k)
+        elif abs(b)>abs(a) and q<len(l):
+            k=k+abs(b)
+            return raw(l,n+1,q+1,k)
+        elif abs(b)==abs(a) and q<len(l):
+            k=k+abs(b)
+            return raw(l,n+1,q+1,k)
+        elif q==len(l)-1 :
+            return k
+    return k
+l=list((eval(input('请输入：'))))
+n=0
+q=1
+k=0
+print(raw(l,n,q,k))
