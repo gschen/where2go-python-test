d4b863d5294c2b387fe657683549fd8d750988fe
刘仕豪 <1600975493@qq.com>
2019-12-16 18:48:48 +0800

    力扣167周赛

--- 1906101041刘仕豪/力扣周赛/167第一题.py
+++ 1906101041刘仕豪/力扣周赛/167第一题.py
@@ -1,7 +1,14 @@
-def getDecimalValue(head) :
-        cur = head
-        ans = 0
-        while cur:
-            ans = ans * 2 + cur.val
-            cur = cur.next
-        return ans
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        res = ''
+        while head:
+            res += str(head.val)
+            head = head.next
+        return int(res, 2)
+    print(getDecimalValue([1,0,1]))
