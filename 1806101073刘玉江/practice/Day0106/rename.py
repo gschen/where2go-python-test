28a6bb2c0cdf87b90866e4e1686eb82692998af4
liuyujiangx <1914571065@qq.com>
2020-01-10 22:06:44 +0800

    更新

--- 1806101073刘玉江/practice/Day0106/rename.py
+++ 1806101073刘玉江/practice/Day0106/rename.py
@@ -7,18 +7,18 @@ def readname():
     for i in range(0, 39):
         name[i] = name[i][28:]
     return name
+
+
 def rename(name):
     filePath = 'C:\\Users\\19145\\Desktop\\作业\\作业10'
     i = 0
     for file in os.listdir(filePath):
-        new_name = file.replace(file,'WEB程序设计2018级信本3班'+name[i])
-        os.rename(os.path.join(filePath,file),os.path.join(filePath,new_name))
-        i=i+1
+        new_name = file.replace(file, 'WEB程序设计2018级信本3班' + name[i])
+        os.rename(os.path.join(filePath, file), os.path.join(filePath, new_name))
+        i = i + 1
     print('end')
 
 
 if __name__ == "__main__":
     name = readname()
     rename(name)
-
-
