d757409761921cbc181239c7aecd1752cc537d9c
陈功锁 <2547253080@qq.com>
2020-02-10 09:25:21 +0800

    test

--- 0002-案例库/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd codelabs
-claat-darwin-amd64 export *.md
-
-cd ..
-gulp serve --codelabs-dir=codelabs
---
+++ 0002-案例库/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CMD_EXPORT=""
+
+case "`uname`" in
+Darwin) CMD_EXPORT=""$CMD_EXPORT"claat-darwin-amd64"
+;;
+Linux) CMD_EXPORT=""$CMD_EXPORT"claat-linux-amd64"
+;;
+esac
+
+CMD_EXPORT=""$CMD_EXPORT" export *.md"
+echo $CMD_EXPORT
+
+cd codelabs
+$CMD_EXPORT
+
+cd ..
+gulp serve --codelabs-dir=codelabs
