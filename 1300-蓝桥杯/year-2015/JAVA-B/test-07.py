3adfebcc04f9b2d1d162971b78df84c77f92adfb
raolongjiang <2126779129@qq.com>
2020-02-15 17:18:53 +0800

    解决了小朋友崇拜圈问题

--- 1300-蓝桥杯/year-2015/JAVA-B/test-07.py
+++ 1300-蓝桥杯/year-2015/JAVA-B/test-07.py
@@ -1,8 +1,55 @@
 #骰子问题
 n, m = input().split()
-def touzi(n,m):
-    lis=[]
-    for i in range(eval(m)):
-        lis.append(list(map(int, input().split(','))))# 用列表将对应的骰子存起来
+n=int(n)
+lis1=[]
+for i in range(eval(m)):
+    lis = list(map(int, input().split()))
+    lis1.append(lis)
+print(lis1)
+def touzi(n):
+    #统计可以自由重叠的骰子组合
+    sun_freedom=0
+    for i in range(1,7):
+        for j in lis1:
+            if i not in j:
+                sun_freedom+=1
+    sum2=(4*6)**n
+    return sum2
+#统计无法自由组合的骰子组合
+def sss(n):
+    if n<=2:
+        sum=1
+        num = 0
+        for i in range(1,7):
+            if i==1:
+                up=4
+            elif i==2:
+                up=5
+            elif i==3:
+                up=6
+            elif i==4:
+                up=1
+            elif i==5:
+                up=2
+            else:
+                up=3
+            for j in lis1:
+                if up not in j:
+                    num+=1
+            print(num)
+        sum=sum*4*num
+print(sss(n))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- 1300-蓝桥杯/year-2018/JAVA-C/test-07.py
+++ 1300-蓝桥杯/year-2018/JAVA-C/test-07.py
@@ -1,7 +1,23 @@
+#小朋友崇拜圈
+#找规律理解崇拜关系,将一行整数存入数组nums，则崇拜关系可表示为：x崇拜nums[x-1];
+#定义变量max保存最长崇拜圈。
+#遍历数组进行寻找，找到崇拜的人存入集合中且集合内未有重复元素则计数器加1
+#每次遍历后比较计数器con和max，将最大的数保存为max。
+#输出max。
+
 m=int(input())
-n=list(map(int,input().split(' ')))
-# print(m,list(n))
-l=[]
-for i in n:
-    x=n[i-1]
-    l.append(x)
+nums=list(map(int,input().split(' ')))
+print(m,list(nums))
+max=0
+for i in range(len(nums)-1):
+    lis = []
+    con=0
+    x=nums[i]
+    while x not in lis :
+        lis.append(x)
+        x=nums[x-1]
+        con+=1
+        if con>max:
+            max=con
+        print(lis)
+print(max)
