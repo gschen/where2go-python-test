e4d5b3e280febf91641c3eb6546994edde416c5f
raolongjiang <2126779129@qq.com>
2020-02-16 13:35:36 +0800

    解决了小朋友崇拜圈问题

--- 1300-蓝桥杯/year-2018/JAVA-C/test-07.py
+++ 1300-蓝桥杯/year-2018/JAVA-C/test-07.py
@@ -5,19 +5,17 @@
 #每次遍历后比较计数器con和max，将最大的数保存为max。
 #输出max。
 
-m=int(input())
-nums=list(map(int,input().split(' ')))
-print(m,list(nums))
-max=0
-for i in range(len(nums)-1):
-    lis = []
-    con=0
+N=int(input())
+nums=list(map(int,input().split(' ')))#将输入的整数存入列表中
+max=0#定义最大人数
+for i in range(len(nums)):
+    lis = []#用列表模仿崇拜圈
+    con=0#定义计数器
     x=nums[i]
-    while x not in lis :
+    while x not in lis :#判断小朋友是否在圈中，若没在则将他加入圈中并进入循环
         lis.append(x)
-        x=nums[x-1]
-        con+=1
-        if con>max:
+        x=nums[x-1]#寻找崇拜者
+        con+=1#计数器加一
+        if con>max:#比较计数器和最大人数，找到最大的圈
             max=con
-        print(lis)
 print(max)
