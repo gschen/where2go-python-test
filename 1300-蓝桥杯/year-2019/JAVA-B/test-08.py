2dbb12c7b96f063446d56d8d150bc89597c927b3
HS123456 <2485026774@qq.com>
2020-02-19 08:20:33 +0800

    代码提交

--- 1300-蓝桥杯/year-2019/JAVA-B/test-08.py
+++ 1300-蓝桥杯/year-2019/JAVA-B/test-08.py
@@ -1,8 +1,26 @@
-def find(K,S):
-    conut=0
-    for i in range(len(S)-5):
-        if S[i:i+5]=='Alice' or S[i:i+5]=='Bob':
-            conut+=1
-    return conut
+K=int(input())
+s=input()
+S=s.split(" ")
+print(S)
+res=0
+for i in range(len(S)):
+    if S[i]=='Alice':
+        for j in range(i+1,len(S)):
+            if S[j]=='Bob' or S[j]=='Bob.':
+                sum=1
+                for k in range(i+1,j):
+                    sum+=len(S[k])+1
+                if sum<=K:
+                    res+=1
 
-print(find(20,'ThisisastoryaboutAliceandBob.AlicewantstosendaprivatemessagetoBob'))
+for i in range(len(S)):
+    if S[i]=='Bob' or S[i]=='Bob.':
+        for j in range(i+1,len(S)):
+            if S[j]=='Alice':
+                sum=1
+                for k in range(i+1,j):
+                    sum+=len(S[k])+1
+                if sum<=K:
+                    res+=1
+
+print(res)
