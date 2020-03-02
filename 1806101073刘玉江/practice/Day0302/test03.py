2987af8614b2c92b55acb14f71926cd1dd7d420f
liuyujiangx <1914571065@qq.com>
2020-03-02 21:06:51 +0800

    更新

--- 1806101073刘玉江/practice/Day0302/test03.py
+++ 1806101073刘玉江/practice/Day0302/test03.py
@@ -15,7 +15,7 @@ def threeSum(nums):
         while(L<R):
             if nums[i]+nums[L]+nums[R] == 0:
                 res.append([nums[i],nums[L],nums[R]])
-                while(L<sR and nums[L] == nums[L+1]):
+                while(L<R and nums[L] == nums[L+1]):
                     L += 1
                 while(L<R and nums[R] == nums[R-1]):
                     R -= 1
